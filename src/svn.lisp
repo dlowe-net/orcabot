caf19892a0bf0c224effb2a8f54ffc2abf1a3b97
Daniel Lowe <dlowe@dlowe.net>
2009-10-23 08:49:22 -0400

    svn command can now look up the last commit that changed a file
    Can also decode html entities for output

--- src/svn.lisp
+++ src/svn.lisp
@@ -11,19 +11,38 @@
       (format nil "~a - ~a" user
               (string-limit (substitute #\space #\newline message) 160)))))
 
+(defun retrieve-svn-last-commit-log (path)
+  (let ((log (with-output-to-string (str)
+                (sb-ext:run-program "/usr/bin/svn"
+                                    `("log" "-l1" ,(format nil "svn+ssh://svn/svn/ita/trunk/~a" path))
+                                    :input nil :output str))))
+    (ppcre:register-groups-bind (rev user message)
+        ((ppcre:create-scanner "-+\\nr(\\d+) \\| (.*) \\| [^(]+\\([^)]+\\) \\| \\d+ lines?\\n\\n(.*?)^-{70,}" :single-line-mode t :multi-line-mode t)
+         log)
+      (format nil "r~a - ~a - ~a (https://svn/trac/changeset/~a)" rev user
+              (string-limit (substitute #\space #\newline message) 160)
+              rev))))
+
+
 (defcommand svn (message directp rev)
   (multiple-value-bind (match regs)
       (ppcre:scan-to-strings "[#r]?(\\d+)" rev)
     (cond
       ((null match)
-       (when directp
-         (reply-to message "I'd rather have a revision number.")))
+       (let ((info (retrieve-svn-last-commit-log rev)))
+         (cond
+           (info
+            (reply-to message
+                      "~a last modified ~a"
+                      rev info))
+           (directp
+            (reply-to message "I'd rather have a revision number or path.")))))
       (t
        (let ((subject (retrieve-svn-log (aref regs 0))))
          (cond
            (subject
             (reply-to message
-                      "svn r~a is ~a (https:///trac/changeset/~a)"
+                      "svn r~a is ~a (https://svn/trac/changeset/~a)"
                       (aref regs 0) subject (aref regs 0)))
            (directp
             (reply-to message "SVN r~a doesn't seem to exist" (aref regs 0)))))))))
