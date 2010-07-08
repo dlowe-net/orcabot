c1b321dd35c58a81213237dcdf2c1a96e407e61e
Daniel Lowe <dlowe@dlowe.net>
2010-07-08 15:33:12 -0400

    Excluded - from matching svn revision

--- src/main.lisp
+++ src/main.lisp
@@ -216,7 +216,7 @@
                        text)
                     (list "bug" bug))
                   (ppcre:register-groups-bind (rev)
-                      ((ppcre:create-scanner "\\b(?:svn [:#]*|r)(\\d+)\\b"
+                      ((ppcre:create-scanner "\\b(?:svn [:#]*|r)(\\d+)(?:$|[^-])"
                                              :case-insensitive-mode t)
                        text)
                     (list "svn" rev))
