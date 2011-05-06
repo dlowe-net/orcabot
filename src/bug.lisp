3e4c2cb641ea7babdec6a0dbf877941e7e1c96c8
Daniel Lowe <dlowe@dlowe.net>
2011-05-06 14:21:28 -0400

    Bug command now works with new ITA bugzilla instance

--- src/bug.lisp
+++ src/bug.lisp
@@ -4,48 +4,47 @@
 
 (defun bugzilla-login ()
   (let ((creds (authentication-credentials "")))
-    (drakma:http-request "http:///index.cgi"
+    (drakma:http-request "https:///index.cgi"
                          :method :post
                          :parameters `(("Bugzilla_login" . ,(getf creds :login))
                                        ("Bugzilla_password" . ,(getf creds :password)))
                          :cookie-jar *bugzilla-cookies*)))
 
-(defun scrape-bug-title (response)
-  (let ((match (nth-value 1 (cl-ppcre:scan-to-strings
-                             "<h2>(.*)</h2>"
-                             response))))
-    (and match (html-entities:decode-entities (aref match 0)))))
-
-(defun scrape-bug-owner (response)
-  (let ((match (nth-value 1 (cl-ppcre:scan-to-strings
-                             "Assigned&nbsp;To</a>:\\s*</b>\\s*</td>\\s*<td>[^&]+&lt;([^&]+)&gt;</td>"
-                             response))))
-    (and match (html-entities:decode-entities (aref match 0)))))
-
-(defun scrape-bug-status (response)
-  (let ((match (nth-value 1 (cl-ppcre:scan-to-strings
-                             "Status</a>:\\s*</b>\\s*</td>\\s*<td>([^<]+)</td>"
-                             response))))
-    (and match (html-entities:decode-entities (aref match 0)))))
-
 (defun retrieve-bug-info (bug)
   (unless (drakma:cookie-jar-cookies *bugzilla-cookies*)
     (bugzilla-login))
-  (multiple-value-bind (response status)
+  (multiple-value-bind (response status headers)
       (drakma:http-request
-       (format nil "http:///show_bug.cgi?id=~a" bug)
+       (format nil "https:///show_bug.cgi?ctype=xml&id=~a" bug)
        :cookie-jar *bugzilla-cookies*)
     (cond
       ((/= status 200)
        nil)
-      ((cl-ppcre:scan "need a legitimate login and password" response)
+      ((and (string= (cdr (assoc :content-type headers))
+                     "text/html; charset=UTF-8")
+            (cl-ppcre:scan "need a legitimate login and password" response))
        (bugzilla-login)
        (retrieve-bug-info bug))
       (t
-       (values
-        (scrape-bug-title response)
-        (scrape-bug-owner response)
-        (scrape-bug-status response))))))
+       (flet ((resolver (pubid sysid)
+                (declare (ignore pubid))
+                (when (puri:uri= sysid
+                                 (puri:parse-uri
+                                  "https:///bugzilla.dtd"))
+                  (open "data/bugzilla.dtd" :element-type '(unsigned-byte 8)))))
+         (let* ((doc (cxml:parse response
+                                 (cxml-dom:make-dom-builder)
+                                 :entity-resolver #'resolver))
+                (title (dom:node-value
+                         (dom:first-child
+                          (elt (dom:get-elements-by-tag-name *bar* "short_desc") 0))))
+                (status (dom:node-value
+                         (dom:first-child
+                          (elt (dom:get-elements-by-tag-name *bar* "bug_status") 0))))
+                (owner (dom:node-value
+                         (dom:first-child
+                          (elt (dom:get-elements-by-tag-name *bar* "assigned_to") 0)))))
+           (values title owner (string-downcase status))))))))
 
 (define-serious-command bug (message directp bug)
   (multiple-value-bind (match regs)
@@ -59,7 +58,7 @@
          (cond
            (subject
             (reply-to message
-                      "bug #~a is ~a [~a/~a] (http://bug/show_bug.cgi?id=~a)"
+                      "bug #~a is ~a [~a/~a] (https://g/show_bug.cgi?id=~a)"
                       (aref regs 0)
                       subject
                       owner
