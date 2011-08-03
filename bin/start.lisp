b00e2382dcdadcecf77c127734ce008a5056f9a2
Daniel Lowe <dlowe@dlowe.net>
2011-08-03 09:27:02 -0400

    Changed irc server to new datacenter

--- bin/start.lisp
+++ bin/start.lisp
@@ -2,6 +2,6 @@
 (require :swank)
 (swank:create-server :port 40050 :style :spawn :dont-close t)
 (require :orca)
-(orca::orca-run "orca" "ita5prattle1.itasoftware.com"
+(orca::orca-run "orca" "ita2prattle1.itasoftware.com"
                 :port 6601
                 :security :ssl)
