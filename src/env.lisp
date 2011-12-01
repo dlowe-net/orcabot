6f99b5ae2a5644bbe696c3aaaa8cea21d196ad29
Daniel Lowe <dlowe@dlowe.net>
2011-12-01 16:58:22 -0500

    Added concept of environment status

--- src/env.lisp
+++ src/env.lisp
@@ -4,6 +4,8 @@
   "List containing the current known environments")
 (defvar *pss-leases* nil
   "Alist containing the leases people have done (NICK ENV ACTIVITY EXPIRE-TIME)")
+(defvar *pss-env-statuses* nil
+  "Alist of environment/status")
 
 (defun save-env-data ()
   (with-open-file (ouf (orca-path "data/pss-envs.lisp")
@@ -15,12 +17,16 @@
     (terpri ouf)
     (write-line ";; Leases" ouf)
     (write *pss-leases* :stream ouf)
+    (terpri ouf)
+    (write-line ";; Statuses" ouf)
+    (write *pss-env-statuses* :stream ouf)
     (terpri ouf)))
 
 (defun load-env-data ()
   (with-open-file (inf (orca-path "data/pss-envs.lisp") :direction :input)
-    (setf *pss-environments* (read inf))
-    (setf *pss-leases* (read inf))))
+    (setf *pss-environments* (read inf nil nil))
+    (setf *pss-leases* (read inf nil nil))
+    (setf *pss-env-statuses* (read inf nil nil))))
 
 (defun expire-env-leases ()
   (let ((now (get-universal-time)))
@@ -55,10 +61,11 @@
                  (#\s num))))))))
 
 (defun query-leases (env-name leases)
-  (let ((matching (remove env-name leases :test-not #'string-equal :key #'cadr)))
+  (let ((matching (remove env-name leases :test-not #'string-equal :key #'cadr))
+        (status (cdr (assoc env-name *pss-env-statuses* :test #'string-equal))))
     (if matching
         (with-output-to-string (str)
-          (format str "~a used by: " env-name)
+          (format str "~a~@[(~a)~] used by: " env-name status)
           (loop
              with end = (last matching)
              for match-el on matching
@@ -69,7 +76,7 @@
                      (describe-time-left (fourth match)))
              (unless (eq match-el end)
                (format str ", "))))
-        (format nil "~a is currently free" env-name))))
+        (format nil "~a~@[(~a)~] is currently free" env-name status))))
 
 (defun remove-all-env-leases (env-name)
   (setf *pss-leases* (remove env-name *pss-leases* :test #'string-equal :key #'second)))
@@ -105,6 +112,23 @@
      (save-env-data)
      (format nil "~a released by ~a" env-name nick))))
 
+(defun set-env-status (env-name status)
+  (cond
+    ((not (member env-name *pss-environments* :test #'string-equal))
+     (format nil "Can't find environment ~a" env-name))
+    ((zerop (length status))
+     (setf *pss-env-statuses* (remove (assoc env-name *pss-env-statuses* :test #'string-equal)
+                                      *pss-env-statuses*))
+     (save-env-data)
+     (format nil "~a status removed" env-name))
+    (t
+     (let ((tuple (assoc env-name *pss-env-statuses* :test #'string-equal)))
+       (if tuple
+           (setf (cdr tuple) status)
+           (setf *pss-env-statuses* (acons env-name status *pss-env-statuses*))))
+     (save-env-data)
+     (format nil "~a status is now: ~a" env-name status))))
+
 (define-serious-command env (message directp &rest args)
   (expire-env-leases)
   (let ((env-name (first args))
@@ -116,11 +140,12 @@
        (flet ((send (text)
                 (irc:notice (connection message) (source message) text)))
          (send "~env                                    - list known environments")
-         (send "~env <envname>                          - query leases for environment")
+         (send "~env <envname>                          - display environment info")
          (send "~env <envname> take <time> [<activity>] - lease the environment")
          (send "~env <envname> release                  - release your lease on environment")
          (send "~env <envname> add                      - add a new environment")
          (send "~env <envname> remove                   - remove an environment")
+         (send "~env <envname> status [<status>]        - set the environment status")
          (send "~env <envname> help                     - display this help")))
       ((null (cdr args))
        ;; query leases
@@ -133,6 +158,8 @@
                                        (join-string #\space (cdddr args)))))
       ((string-equal subcmd "release")
        (reply-to message (release-lease env-name (source message))))
+      ((string-equal subcmd "status")
+       (reply-to message (set-env-status env-name (join-string #\space (cddr args)))))
       ((string-equal subcmd "add")
        (cond
          ((member env-name *pss-environments* :test #'string-equal)
