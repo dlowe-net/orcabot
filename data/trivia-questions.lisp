918aeddba935dcc7ec5d4a25ec1e22c182877d22
Daniel Lowe <dlowe@dlowe.net>
2012-01-26 21:49:55 -0500

    Updated trivia questions to new format

--- data/trivia-questions.lisp
+++ data/trivia-questions.lisp
@@ -1,62 +1,100 @@
-(("What does RAM stand for?" "random access memory")
- ("What video game features a spiky-headed main character named Cloud?"
+((1 "What does RAM stand for?" "random access memory")
+ (2 "What video game features a spiky-headed main character named Cloud?"
   "final fantasy vii" "final fantasy")
- ("What color lightsaber does Master Windu possess in Star Wars?" "purple"
+ (3 "What color lightsaber does Master Windu possess in Star Wars?" "purple"
   "violet")
- ("What is the name of Tony Stark's army friend?" "rhodey" "james rhodes"
+ (4 "What is the name of Tony Stark's army friend?" "rhodey" "james rhodes"
   "lieutenant colonal james rhodes")
- ("Besides Griffendor, Slytherin, and Ravenclaw, what is the remaining house in Hogwarts that students belong to?"
+ (5
+  "Besides Griffendor, Slytherin, and Ravenclaw, what is the remaining house in Hogwarts that students belong to?"
   "hufflepuff")
- ("Exactly how old is Aragorn, in years, when Frodo meets him in the Prancing Pony?"
+ (6
+  "Exactly how old is Aragorn, in years, when Frodo meets him in the Prancing Pony?"
   "87" "eighty-seven" "eighty seven")
- ("Roughly, how many seconds does it take, on average, for sunlight from the Sun take to reach Earth?"
+ (7
+  "Roughly, how many seconds does it take, on average, for sunlight from the Sun take to reach Earth?"
   "499" "500")
- ("What does the acronym LED stand for?" "light emitting diode")
- ("What video game is often referred to as DOA?" "dead or alive")
- ("What mock artist created the songs \"White and Nerdy\" and \"Buy Me a Condo\"?"
+ (8 "What does the acronym LED stand for?" "light emitting diode")
+ (9
+  "What mock artist created the songs \"White and Nerdy\" and \"Buy Me a Condo\"?"
   "weird al" "al yankovic" "weird al yankovic")
- ("Who did Sir Gawain behead in the classic Pearl Poet poem?" "Green knight"
+ (10 "Who did Sir Gawain behead in the classic Pearl Poet poem?" "Green knight"
   "green knight")
- ("What gas must your SCVs mine if you want to raise an army of Firebats to fight the Zerg"
+ (11
+  "What gas must your SCVs mine if you want to raise an army of Firebats to fight the Zerg"
   "vespene" "vespene gas")
- ("The martial art practiced by the water benders is based on what real-world martial art from China?"
+ (12
+  "The martial art practiced by the water benders is based on what real-world martial art from China?"
   "tai chi")
- ("How many bytes are in a mibibyte?" "1048576" "2^20" "2**20")
- ("What element is used to regulate the flow of antimatter in a Federation starship?"
+ (13
+  "The martial art practiced by the fire benders is based on what real-world martial art from China?"
+  "shaolin")
+ (14 "How many bytes are in a mibibyte?" "1048576" "2^20" "2**20")
+ (15
+  "What element is used to regulate the flow of antimatter in a Federation starship?"
   "dilithium")
- ("How many gigawatts of electricity are required to power Doc Brown's time machine?"
+ (16
+  "How many gigawatts of electricity are required to power Doc Brown's time machine?"
   "1.21")
- ("In the new BSG, what Colonial technology detects, identifies, and tracks objects, analogous in function to RADAR?"
+ (17
+  "In the new BSG, what Colonial technology detects, identifies, and tracks objects, analogous in function to RADAR?"
   "dradis")
- ("In Ender's Game, which way is the enemy base?" "down")
- ("Which character's smiley face button is used as the logo for Watchmen?" "comedian")
- ("who takes Frodo across the ford to Rivendell?" "glorfindel")
- ("According to the shopkeeper in the Monty Python sketch, what is the parrot pining for?"
+ (18 "In Ender's Game, which way is the enemy base?" "down")
+ (19 "Which character's smiley face button is used as the logo for Watchmen?"
+  "comedian")
+ (20 "who takes Frodo across the ford to Rivendell?" "shadowfax")
+ (21
+  "According to the shopkeeper in the Monty Python sketch, what is the parrot pining for?"
   "fjords")
- ("What was the HTC code name for the G1, the first Android phone with a traditional release?"
+ (22
+  "What was the HTC code name for the G1, the first Android phone with a traditional release?"
   "dream")
- ("Name the video game: \"You have died of dysentery.\"" "oregon trail")
- ("Who is Gimli's father" "gloin")
- ("H2SO4 is the chemical formula for which deadly liquid?" "sulphuric acid")
- ("What \"black metal\" gave blacksmiths their name?" "iron")
- ("What planet needs 248 years to meander its way around the sun?" "pluto")
- ("What did 18th-century astronomer Edmund Halley chart 24 of?" "comets")
- ("What planet is named after the Greek god who personified the sky?" "uranus")
- ("What's the positively charged particle in the nucleus of an atom called?" "proton")
- ("What cartoonist has had three insect species named after him?"
+ (23 "Name the video game: \"You have died of dysentery.\"" "oregon trail")
+ (24 "Who is Gimli's father" "gloin")
+ (25 "H2SO4 is the chemical formula for which deadly liquid?" "sulfuric acid"
+  "sulphuric acid")
+ (26 "What did 18th-century astronomer Edmund Halley chart 24 of?" "comets")
+ (27 "What planet is named after the Greek god who personified the sky?"
+  "uranus")
+ (28 "What's the positively charged particle in the nucleus of an atom called?"
+  "proton")
+ (29 "What cartoonist has had three insect species named after him?"
   "gary larson")
- ("What did William Stanley invent in 1885 to transfer the current of one circuit to another?"
+ (30
+  "What did William Stanley invent in 1885 to transfer the current of one circuit to another?"
   "transformer")
- ("What country was India ink developed in?" "china")
- ("What fiber-optic instrument allows surgeons to see and repair damage within joints?"
+ (31 "What country was India ink developed in?" "china")
+ (32
+  "What fiber-optic instrument allows surgeons to see and repair damage within joints?"
   "arthroscope")
- ("What time period is sandwiched between the Cretaceous and Triassic?"
+ (33 "What time period is sandwiched between the Cretaceous and Triassic?"
   "jurassic")
- ("What colorless, odorless substance is the main constituent of natural gas?"
+ (34
+  "What colorless, odorless substance is the main constituent of natural gas?"
   "methane")
- ("What facial features flank your glabella?" "eyebrows")
- ("What bot is the most awesome?" "orca" "manatee" "narwhal")
- ("What radioactive element is extracted from carnotite and pitchblende"
+ (35 "What facial features flank your glabella?" "eyebrows")
+ (36 "What radioactive element is extracted from carnotite and pitchblende"
   "uranium")
- ("Ganymede is a moon of which planet" "jupiter")
- ("What is the farthest human-made object in the universe" "voyager 1"))
+ (37 "Ganymede is a moon of which planet" "jupiter")
+ (38 "What is the farthest human-made object in the universe" "voyager 1")
+ (39 "Into how many zones are DVD regions divided?" "eight")
+ (40 "What is the fictional branch of science from Asimov's Foundation series devoted to predicting the future?" "psychohistory" "psycho-history")
+ (41 "In what year was Google founded?" "1998")
+ (42 "What does the K stand for in the K&R book?" "Kernighan")
+ (43 "in \"2001: a Space Odyssey,\" what is the name of the song HAL sings as he is being shut down?" "Daisy Bell")
+ (44 "What's the most common isotope for naturally occuring uranium?" "238" "u238")
+ (45 "How much data can Johnny Mnemonic carry inside his brain?" "320GB" "320g" "320 gigabytes")
+ (46 "What is the common name for IEEE 1394?" "Firewire")
+ (47 "What does the B stand for in BSD Unix?" "Berkeley")
+ (48 "With what words does the main villain in 1982 Tron end conversations?" "eol" "end of line")
+ (49 "What does JPSC stand for?" "joint passenger services conference")
+ (50 "What does SSIM stand for?" "standard schedules information manual")
+ (52 "What does IATCI stand for?" "interairline through check in")
+ (53 "What is the name of the alien force attacking humanity in the beginning of Halo 1?" "the Covenant")
+ (54 "What essential mineral is also a key component in password security?" "salt")
+ (55 "What is Rule #1?" "Don't get Dan fired")
+ (56 "What was the first major website to announce they would modify their site or black out on January 18, 2012, to protest the SOPA and PIPA bills?" "reddit")
+ (57 "What is Cape Air's production teletype address?" "HDQRM9K")
+ (58 "What, according to Egon, happens if you cross the streams?" "a complete protonic reversal")
+ (59 "What function uses PADIS EDIFACT message types with names beginning in \"TKC\"?" "ticket control")
+ (60 "Who developed the first LISP programming language in 1958?" "john mccarthy"))
