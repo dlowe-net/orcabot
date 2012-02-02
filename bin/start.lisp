88a09d1da03c69dce321961fe040a46cc97cbdb2
Daniel Lowe <dlowe@dlowe.net>
2012-02-02 14:28:39 -0500

    Fixed invocation of orca in start.lisp file

--- bin/start.lisp
+++ bin/start.lisp
@@ -15,5 +15,5 @@
 (require :asdf)
 (require :swank)
 (swank:create-server :port 40050 :style :spawn :dont-close t)
-(require :orca)
-(orca::background-orca-session (second *posix-argv*))
+(require :orcabot)
+(orcabot::background-orcabot-session (second *posix-argv*))
