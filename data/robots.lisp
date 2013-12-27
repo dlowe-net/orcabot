b6ddd4d2e35aab227133d3af2e0bd4878fa651bc
Daniel Lowe <dlowe@dlowe.net>
2013-12-27 16:03:40 -0500

    Add ABBREV module and sample robot name generator command

---
+++ data/robots.lisp
@@ -0,0 +1,225 @@
+(thing-adj
+ "advanced"
+ "artificial"
+ "biomechanical"
+ "bionic"
+ "cybernetic"
+ "clockwork"
+ "digital"
+ "electronic"
+ "forbidden"
+ "functional"
+ "global"
+ "hydraulic"
+ "intelligent"
+ "inhuman"
+ "journeying"
+ "kinetic"
+ "knowledgable"
+ "lifelike"
+ "mechanical"
+ "networked"
+ "obedient"
+ "positronic"
+ "quantum"
+ "robotic"
+ "synthetic"
+ "transforming"
+ "upgraded"
+ "vigilant"
+ "versatile"
+ "wireless"
+ "xperimental"
+ "xtraterrestrial"
+ "ytterbium"
+ "zeta")
+
+(activity-adj
+ "accurate"
+ "basic"
+ "ceaseless"
+ "dangerous"
+ "efficient"
+ "forbidden"
+ "galactic"
+ "hazardous"
+ "immediate"
+ "journeying"
+ "justified"
+ "jeopardous"
+ "kamikaze"
+ "logical"
+ "mechanical"
+ "mandatory"
+ "intensive"
+ "nocturnal"
+ "online"
+ "potential"
+ "rational"
+ "scientific"
+ "terran"
+ "ultimate"
+ "worldwide"
+ "widespread"
+ "xperimental"
+ "xpert"
+ "yearly"
+ "yucky"
+ "zealous")
+
+(activity
+ "analysis"
+ "assassination"
+ "battle"
+ "bioscience"
+ "calculation"
+ "capture"
+ "destruction"
+ "exploration"
+ "fighting"
+ "fun"
+ "geophysics"
+ "gratification"
+ "gunfighting"
+ "harm"
+ "infiltration"
+ "investigation"
+ "judo"
+ "jobs"
+ "jealousy"
+ "killing"
+ "kindness"
+ "learning"
+ "mathematics"
+ "nullification"
+ "obliteration"
+ "observation"
+ "peacekeeping"
+ "patrolling"
+ "questioning"
+ "quantification"
+ "repair"
+ "sabotage"
+ "troubleshooting"
+ "utility"
+ "violence"
+ "vengeance"
+ "warfare"
+ "wrecking"
+ "wayfaring"
+ "xenocide"
+ "xecution"
+ "yelling"
+ "yardwork"
+ "zoology"
+ "zymurgy"
+)
+
+
+(for
+ "assembled for"
+ "built for"
+ "calibrated for"
+ "designed for"
+ "engineered for"
+ "fabricated for"
+ "generated for"
+ "hardwired for"
+ "intended for"
+ "justified for"
+ "keen on"
+ "limited to"
+ "manufactured for"
+ "normally for"
+ "optimized for"
+ "programmed for"
+ "qualified for"
+ "responsible for"
+ "skilled in"
+ "trained for"
+ "used for"
+ "viable for"
+ "wanting"
+ "xperienced in"
+ "yearning for"
+ "zoned for")
+
+(thing
+ "android"
+ "being"
+ "construct"
+ "device"
+ "entity"
+ "facsimile"
+ "guardian"
+ "humanoid"
+ "individual"
+ "judge"
+ "juggernaut"
+ "knight"
+ "lifeform"
+ "machine"
+ "neohuman"
+ "organism"
+ "person"
+ "quadruplet"
+ "replicant"
+ "soldier"
+ "technician"
+ "unit"
+ "varient"
+ "worker"
+ "xenomorph"
+ "youth"
+ "zombie")
+
+;; 1
+(phrase thing)
+
+;; 2
+(phrase thing-adj thing)
+
+;; 3
+(phrase activity-adj activity thing)
+(phrase activity "and" activity thing)
+
+;; 4
+(phrase activity "and" activity-adj activity thing)
+(phrase activity-adj activity-adj activity thing)
+(phrase activity-adj activity "and" activity thing)
+(phrase thing-adj thing for activity)
+(phrase thing for activity "and" activity)
+(phrase thing-adj activity-adj activity thing)
+(phrase thing for activity-adj activity)
+
+;; 5
+(phrase thing-adj thing-adj thing for activity)
+(phrase thing-adj thing for activity "and" activity)
+(phrase thing-adj thing for activity-adj activity)
+(phrase thing for activity "and" activity-adj activity)
+
+;; 6
+(phrase activity-adj activity-adj activity "and" activity-adj activity thing)
+(phrase thing for activity "," activity-adj activity "and" activity-adj activity)
+(phrase thing-adj thing for activity "and" activity-adj activity)
+(phrase thing-adj thing-adj activity "and" activity-adj activity thing)
+(phrase thing-adj thing-adj thing for activity "and" activity-adj activity)
+(phrase thing-adj thing-adj thing for activity-adj activity "and" activity-adj activity)
+
+;; 7
+(phrase thing-adj thing-adj thing for activity-adj activity "and" activity)
+(phrase thing-adj thing-adj thing for activity "and" activity-adj activity)
+
+;; 8
+(phrase thing-adj thing-adj thing for activity-adj activity "and" activity-adj activity)
+
+;; 9
+(phrase thing-adj thing-adj thing for activity-adj activity "and" thing-adj activity-adj activity)
+
+;; 10
+(phrase thing-adj thing-adj thing for activity-adj activity "," activity-adj activity "and" activity-adj activity)
+
+;; 11
+(phrase thing for activity "and" activity-adj activity "/" thing-adj thing for activity "and" activity-adj activity)
+(phrase thing-adj thing for activity "and" activity "/" thing-adj thing for activity-adj activity "and" activity)
+(phrase thing-adj thing for activity "and" activity "/" thing-adj thing for activity "and" activity-adj activity)
---
+++ src/abbrev.lisp
@@ -0,0 +1,97 @@
+(in-package #:orcabot)
+
+;; The abbreviation db has two parts - WORDS and PHRASES
+;;   WORDS are a list of (kind . word) pairs.
+;;   PHRASES are a list of phrases, and a phrase is a list of word kinds.
+
+(defclass abbrev-db ()
+  (words phrases))
+
+(defun load-abbrev-db (path)
+  (with-open-file (inf path :direction :input)
+    (let ((result (make-instance 'abbrev-db )))
+      (with-slots (words phrases) result
+        (setf words nil
+              phrases nil)
+        (loop
+           for term = (read inf nil)
+           while term
+           do
+             (case (car term)
+               (phrase
+                (push (cdr term) phrases))
+               (t
+                (dolist (word (cdr term))
+                  (push (cons (car term) word)
+                        words)))))
+        result))))
+
+(defun count-syms (list)
+  (count-if #'symbolp list))
+
+(defun potential-phrases (db wanted-len)
+  (with-slots (phrases) db
+    (alexandria:shuffle (remove wanted-len phrases :key #'count-syms :test-not #'=))))
+
+(defun select-word (db kind letter used)
+  (let ((candidates (with-slots (words) db
+                      (loop
+                         for word in words
+                         when (and (eql (car word) kind)
+                                   (char-equal (char (cdr word) 0) letter)
+                                   (not (gethash (cdr word) used)))
+                         collect (cdr word)))))
+    (cond
+      (candidates
+       (alexandria:random-elt candidates))
+      (t
+       (format t "ERR: No unused word for letter '~a' and type ~a~%" letter kind)
+       nil))))
+
+(defun render-phrase-template (db template abbrev)
+  (let ((result nil)
+        (used (make-hash-table :test 'equal)))
+    (loop
+       with letter-idx = 0
+       for word-kind in template
+       for letter = (char abbrev letter-idx)
+       do
+         (etypecase word-kind
+           (string
+            (push word-kind result))
+           (symbol
+            (let ((word (select-word db word-kind letter used)))
+              (unless word
+                (return nil))
+              (push (string-capitalize word :end 1) result)
+              (setf (gethash word used) t)
+              (incf letter-idx))))
+       finally (return (nreverse result)))))
+
+(defun expand-abbrev (db abbrev)
+  (loop
+     for phrase-template in (potential-phrases db (length abbrev))
+     as phrase = (render-phrase-template db phrase-template abbrev)
+     until phrase
+     finally (return (and phrase
+                          (join-to-string #\space phrase)))))
+
+(defmodule abbrev abbrev-module ("robot")
+  (robot-db :accessor robot-db-of))
+
+(defmethod initialize-module ((module abbrev-module) config)
+  (setf (robot-db-of module) (load-abbrev-db (static-path "robots.lisp"))))
+
+(defmethod handle-command ((module abbrev-module)
+                           (cmd (eql 'robot))
+                           message args)
+  "robot <word> - generate robot name from word (max 11 chars)"
+  (cond
+    ((null args)
+     (reply-to message "Usage: .robot <word>"))
+    ((> (length (first args)) 11)
+     (reply-to message "The word must be less than 11 characters."))
+    (t
+     (reply-to message "~a: ~a~%"
+               (first args)
+               (expand-abbrev (robot-db-of module) (first args))))))
