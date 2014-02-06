78c3bffe784c88f0bd6361288a1058e308c5b6cb
Daniel Lowe <dlowe@dlowe.net>
2014-02-06 14:53:33 -0500

    Improve valsi command with component destructuring and other fields

--- src/lojban.lisp
+++ src/lojban.lisp
@@ -3,88 +3,119 @@
 (defclass lojban-word ()
   ((text :reader text-of :initarg :text)
    (category :reader category-of :initarg :category)
+   (selmaho :reader selmaho-of :initarg :selmaho)
    (definition :reader definition-of :initarg :definition)
    (notes :reader notes-of :initarg :notes)
-   (rafsi :reader rafsi-of :initarg :rafsi)))
+   (affixes :reader affixes-of :initarg :affixes)))
+
+(defclass lojban-db ()
+  ((valsi :reader valsi-of :initform (make-hash-table :test 'equal))
+   (affixes :reader affixes-of :initform (make-hash-table :test 'equal))
+   (glosses :reader glosses-of :initform (make-hash-table :test 'equal))))
 
 (defclass jbovlaste-sax-handler (sax:abstract-handler)
-  ((table :reader table-of :initarg :table)
+  ((db :reader db-of :initarg :db)
    text-handler
-   last-word last-type last-def last-notes last-rafsi))
+   last-word last-type last-def last-notes last-selmaho last-affix
+   affixes-list))
 
 (defmethod sax:start-document ((handler jbovlaste-sax-handler))
-  (with-slots (text-handler last-word last-type last-def last-notes last-rafsi) handler
+  (with-slots (text-handler last-word last-type last-def last-notes last-selmaho last-affix) handler
     (setf last-word nil
           last-type nil
           text-handler nil
           last-def nil
           last-notes nil
-          last-rafsi nil)))
+          last-selmaho nil
+          last-affix nil)))
 
 (defmethod sax:end-document ((handler jbovlaste-sax-handler))
   nil)
 
 (defmethod sax:start-element ((handler jbovlaste-sax-handler) namespace-uri local-name qname attributes)
-  (with-slots (table text-handler last-word last-type last-def last-notes last-rafsi) handler
+  (with-slots (db text-handler last-word last-type last-def last-notes last-selmaho last-affix affixes-list) handler
     (cond
       ((string= local-name "valsi")
        (setf last-word (sax:attribute-value (sax:find-attribute "word" attributes))
              last-type (sax:attribute-value (sax:find-attribute "type" attributes))
              text-handler nil
-             last-def nil
-             last-notes nil
-             last-rafsi nil))
+             last-def ""
+             last-notes ""
+             last-selmaho ""
+             last-affix ""
+             affixes-list nil))
       ((string= local-name "definition")
        (setf text-handler 'def))
       ((string= local-name "notes")
        (setf text-handler 'notes))
+      ((string= local-name "selmaho")
+       (setf text-handler 'selmaho))
       ((string= local-name "rafsi")
-       (setf text-handler 'rafsi))
+       (setf text-handler 'affixes))
       ((string= local-name "nlword")
-       (setf (gethash (sax:attribute-value (sax:find-attribute "word" attributes)) table)
-             (gethash (sax:attribute-value (sax:find-attribute "valsi" attributes)) table))))))
+       (setf (gethash (sax:attribute-value (sax:find-attribute "word" attributes)) (glosses-of db))
+             (gethash (sax:attribute-value (sax:find-attribute "valsi" attributes)) (valsi-of db)))))))
 
 (defmethod sax:characters ((handler jbovlaste-sax-handler) data)
-  (with-slots (text-handler last-def last-notes last-rafsi)
-      handler
-    (case text-handler
-      (def
-       (setf last-def (ppcre:regex-replace-all "\\$?([a-z])_\\{?(\\d+)\\}?\\$?" data "\\1\\2")
-             text-handler nil))
-      (notes
-       (setf last-notes data
-             text-handler nil))
-      (rafsi
-       (push data last-rafsi)
-       (setf text-handler nil)))))
+  (flet ((append-and-format (str1 str2)
+           (concatenate 'string
+                        str1
+                        (ppcre:regex-replace-all "\\$?([a-z]+)_\\{?(\\d+)\\}?\\$?"
+                                                 str2
+                                                 "\\1\\2"))))
+    (with-slots (text-handler last-def last-notes last-selmaho last-affix)
+        handler
+      (case text-handler
+        (def
+         (setf last-def (append-and-format last-def data)))
+        (notes
+         (setf last-notes (append-and-format last-notes data)))
+        (selmaho
+         (setf last-selmaho (append-and-format last-selmaho data)))
+        (affixes
+         (setf last-affix
+               (concatenate 'string last-affix data)))))))
 
 (defmethod sax:end-element ((handler jbovlaste-sax-handler) namespace-uri local-name qname)
   (cond
     ((string= local-name "valsi")
-     (with-slots (table last-word last-type last-def last-notes last-rafsi) handler
-       (push (make-instance 'lojban-word
-                            :text last-word
-                            :category last-type
-                            :definition last-def
-                            :notes last-notes
-                            :rafsi last-rafsi)
-             (gethash last-word table))))))
+     (with-slots (db last-word last-type last-def last-notes last-selmaho affixes-list) handler
+       (let ((word (make-instance 'lojban-word
+                                  :text last-word
+                                  :category last-type
+                                  :selmaho (when (string/= last-selmaho "") last-selmaho)
+                                  :definition last-def
+                                  :notes last-notes
+                                  :affixes affixes-list)))
+         (push word (gethash last-word (valsi-of db)))
+         (dolist (affix affixes-list)
+           (setf (gethash affix (affixes-of db)) word))
+         (when (string= (category-of word) "gismu")
+           (setf (gethash (text-of word) (affixes-of db)) word)
+           (setf (gethash (subseq (text-of word) 0 4) (affixes-of db)) word)
+           (setf (gethash (text-of word) (affixes-of db)) word)))))
+    ((member local-name '("definition" "notes" "selmaho") :test #'string=)
+     (with-slots (text-handler) handler
+       (setf text-handler nil)))
+    ((string= local-name "rafsi")
+     (with-slots (last-affix affixes-list) handler
+       (push last-affix affixes-list)
+       (setf last-affix "")))))
 
 (defun load-lojban-dictionary (path)
-  (let ((table (make-hash-table :test 'equalp)))
-    (with-open-file (inf path :element-type '(unsigned-byte 8))
-      (cxml:parse inf (make-instance 'jbovlaste-sax-handler :table table)))
-    (maphash (lambda (k v)
-               (declare (ignore k))
-               (dolist (word v)
-                 (dolist (one-rafsi (rafsi-of word))
-                   (unless (gethash one-rafsi table)
-                     (pushnew word (gethash one-rafsi table))))))
-             table)
-    table))
+  (let ((db (make-instance 'lojban-db)))
+    (cxml:parse-file path (make-instance 'jbovlaste-sax-handler :db db))
+    db))
 
-(defun lojban-lookup (table term)
-  (gethash term table))
+(defun lojban-lookup (db raw-term)
+  (let ((term (string-downcase raw-term)))
+    (remove-duplicates
+     (append
+      (let ((result (gethash term (glosses-of db))))
+        (when result (list result)))
+      (let ((result (gethash term (affixes-of db))))
+        (when result (list result)))
+      (gethash term (valsi-of db))))))
 
 (defmodule lojban lojban-module ("valsi" "gerna")
   (dict :accessor dict-of))
@@ -97,20 +128,113 @@
                   (static-path "jbovlaste-en.xml")))
   (setf (dict-of module) (load-lojban-dictionary (static-path "jbovlaste-en.xml"))))
 
+(defun compound-to-affixes (term)
+  (let* ((c "[bcdfgjklmnprstvxz]")
+         (v "[aeiou]")
+         (cc "(?:bl|br|
+                cf|ck|cl|cm|cn|cp|cr|ct|
+                dj|dr|dz|fl|fr|gl|gr|
+                jb|jd|jg|jm|jv|kl|kr|
+                ml|mr|pl|pr|
+                sf|sk|sl|sm|sn|sp|sr|st|
+                tc|tr|ts|vl|vr|xl|xr|
+                zb|zd|zg|zm|zv)")
+         (vv "(?:ai|ei|oi|au)")
+         (affixes3v (concatenate 'string "(?:" cc v "|" c vv "|" c v "'" v ")"))
+         (affixes3 (concatenate 'string "(?:" affixes3v "|" c v c ")"))
+         (affixes4 (concatenate 'string "(?:" c v c c "|" cc  v  c ")"))
+         (affixes5 (concatenate 'string affixes4 v)))
+
+    (loop for i from 1 upto (1+ (floor (length term)
+                                       3))
+         as reg = (format nil "~v@{(?:(~a)([nry])?|(~a)(y))~1@*~}" i affixes3 affixes4)
+         as reg2 = (concatenate 'string "^" reg "(" affixes3v "|" affixes5 ")$")
+         as matches = (multiple-value-list
+                       (re:scan-to-strings (re:create-scanner reg2 :extended-mode t)
+                                           term
+                                           :sharedp t))
+         when (first matches)
+         do (return-from compound-to-affixes
+              (remove nil (second matches))))
+    nil))
+
+(defun find-lojban-components (dict term)
+  (map 'list (lambda (aff)
+               (when aff
+                 (let ((word (gethash aff (affixes-of dict))))
+                   (and word (text-of word)))))
+       (compound-to-affixes term)))
+
+(defun parse-valsi-args (raw-args)
+  (let (opts args)
+    (dolist (arg raw-args)
+      (if (string= "-" arg :end2 1)
+          (push arg opts)
+          (push arg args)))
+    (values (join-to-string " " args)
+            (find "--affixes" opts :test #'string=)
+            (find "--notes" opts :test #'string=)
+            (find "--class" opts :test #'string=)
+            (find "--type" opts :test #'string=)
+            (find "--components" opts :test #'string=))))
+
 (defmethod handle-command ((module lojban-module)
                            (cmd (eql 'valsi))
-                           message args)
-  "valsi - translate between lojban and english"
-  (let ((results (lojban-lookup (dict-of module) (first args))))
-    (cond
-      ((null results)
-       (reply-to message "~a not found." (first args)))
-      ((endp (cdr results))
-       (reply-to message "~a = ~a" (text-of (first results))
-                 (definition-of (first results))))
-      (t
-       (reply-to message "Results for ~a: ~{~a~^, ~}" (first args)
-                 (mapcar #'text-of results))))))
+                           message raw-args)
+  "valsi [--components|--affixes|--class|--type|--notes] - translate between lojban and english"
+  (multiple-value-bind (term affixesp notesp selmahop typep componentsp)
+      (parse-valsi-args raw-args)
+    (if componentsp
+        (let ((words (find-lojban-components (dict-of module) term)))
+          (cond
+            ((null words)
+             (reply-to message "not found."))
+            ((some #'null words)
+             (reply-to message "not found."))
+            (t
+             (reply-to message "~a (components) = ~{~a~^ ~}" term words))))
+
+        (let* ((results (lojban-lookup (dict-of module) term))
+               (entry (first results)))
+          (cond
+            ((null results)
+             (reply-to message "no results." term))
+            ((cdr results)
+             (reply-to message "~a result~:p for ~a: ~10{~a~^, ~}~a"
+                       term
+                       (mapcar #'text-of results)
+                       (if (> (length results) 10)
+                           "..."
+                           "")))
+            (affixesp
+             (let ((word entry))
+               (reply-to message "~a (affixes) = ~{~a~^, ~}"
+                         (text-of word)
+                         (affixes-of word))))
+            (selmahop
+             (let ((word entry))
+               (if (selmaho-of word)
+                   (reply-to message "~a (class) = ~a"
+                             (text-of word)
+                             (selmaho-of word))
+                   (reply-to message "none found."
+                             (text-of word)
+                             (selmaho-of word)))))
+            (typep
+             (let ((word entry))
+               (reply-to message "~a (type) = ~a"
+                         (text-of word)
+                         (category-of word))))
+            (notesp
+             (let ((word entry))
+               (reply-to message "~a (notes) = ~a"
+                         (text-of word)
+                         (notes-of word))))
+            (t
+             (let ((word entry))
+               (reply-to message "~a = ~a"
+                         (text-of word)
+                         (definition-of word)))))))))
 
 (defun jbofihe (input)
   (let ((out (make-string-output-stream))
