f1925d50733dda5504e0565046ab41cfc92b8cfd
Daniel Lowe <dlowe@dlowe.net>
2014-03-06 10:41:59 -0500

    Improve BTC command robustness and add new sub-BTC unit currencies

--- src/bitcoin.lisp
+++ src/bitcoin.lisp
@@ -34,27 +34,35 @@
     (mapcar (alexandria:compose #'symbol-name #'first) ticker)))
 
 (defun bitcoin-retrieve-currency-info (currency)
-  (if (string= currency "BTC")
-      (values 1 1 1 1)
-      (let* ((ticker (bitcoin-request "http://blockchain.info/ticker"))
-             (info (rest (assoc (string-upcase currency)
-                                ticker
-                                :key #'symbol-name
-                                :test #'string=))))
-        (unless info
-          (error 'bitcoin-error :message (format nil "Currency ~a not supported." currency)))
-        (values
-         (cdr (assoc :|15M| info))
-         (cdr (assoc :last info))
-         (cdr (assoc :buy info))
-         (cdr (assoc :sell info))))))
+  (cond
+    ((string= currency "BTC")
+     (values 1 1 1 1))
+    ((string= currency "MBTC")
+     (values 1000 1000 1000 1000))
+    ((string= currency "UBTC")
+     (values 1000000 1000000 1000000 1000000))
+    ((string= currency "SATOSHI")
+     (values 100000000 100000000 100000000 100000000))
+    (t
+     (let* ((ticker (bitcoin-request "http://blockchain.info/ticker"))
+            (info (rest (assoc (string-upcase currency)
+                               ticker
+                               :key #'symbol-name
+                               :test #'string=))))
+       (unless info
+         (error 'bitcoin-error :message (format nil "Currency ~a not supported." currency)))
+       (values
+        (cdr (assoc :|15M| info))
+        (cdr (assoc :last info))
+        (cdr (assoc :buy info))
+        (cdr (assoc :sell info)))))))
 
 (defun bitcoin-exchange (amount from to)
   (let* ((from-rate (bitcoin-retrieve-currency-info from))
          (to-rate (bitcoin-retrieve-currency-info to)))
     (when (or (null from-rate) (null to-rate))
       (error 'bitcoin-error :message "Currency not supported."))
-    (* (/ amount from-rate) to-rate)))
+    (float (* (/ amount (rationalize from-rate)) (rationalize to-rate)))))
 
 (defmodule bitcoin bitcoin-module ("btc"))
 
@@ -87,11 +95,11 @@
          ;; BTC, then convert $1 USD to BTC.
          (cond
            ((string-equal (second args) "btc")
-            (reply-to message "~a BTC => ~a USD"
+            (reply-to message "~a BTC => ~f USD"
                       (first args)
                       (bitcoin-exchange (parse-number (first args)) "BTC" "USD")))
            (t
-            (reply-to message "~a ~a => ~a BTC"
+            (reply-to message "~a ~a => ~f BTC"
                       (first args)
                       (string-upcase (second args))
                       (bitcoin-exchange (parse-number (first args))
@@ -101,7 +109,7 @@
          ;; three arguments - convert $1 of $2 currency into $3 currency
          (let ((from-currency (string-upcase (second args)))
                (to-currency (string-upcase (third args))))
-           (reply-to message "~a ~a => ~a ~a"
+           (reply-to message "~a ~a => ~f ~a"
                      (first args)
                      from-currency
                      (bitcoin-exchange (parse-number (first args))
@@ -110,5 +118,9 @@
                      to-currency))))
     (bitcoin-error (e)
       (reply-to message "Error: ~a" (message-of e)))
+    (simple-type-error (e)
+      (declare (ignore e))
+      ;; triggered by floating point overflow in sbcl
+      (reply-to message "Error: Amount is completely insane"))
     (parse-error (e)
       (reply-to message "~a." e))))
