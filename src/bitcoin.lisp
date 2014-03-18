6ae759feb27470ba9bc63e839bad98aa55ff89be
Daniel Lowe <dlowe@dlowe.net>
2014-03-18 10:13:34 -0400

    Bitcoin module now only displays btc market data

--- src/bitcoin.lisp
+++ src/bitcoin.lisp
@@ -29,98 +29,27 @@
                    (result (json:decode-json-from-string response)))
               (setf *bitcoin-request-cache* result)))))))))
 
-(defun bitcoin-retrieve-currencies ()
-  (let ((ticker (bitcoin-request "http://blockchain.info/ticker")))
-    (mapcar (alexandria:compose #'symbol-name #'first) ticker)))
-
-(defun bitcoin-retrieve-currency-info (currency)
-  (cond
-    ((string= currency "BTC")
-     (values 1 1 1 1))
-    ((string= currency "MBTC")
-     (values 1000 1000 1000 1000))
-    ((string= currency "UBTC")
-     (values 1000000 1000000 1000000 1000000))
-    ((string= currency "SATOSHI")
-     (values 100000000 100000000 100000000 100000000))
-    (t
-     (let* ((ticker (bitcoin-request "http://blockchain.info/ticker"))
-            (info (rest (assoc (string-upcase currency)
-                               ticker
-                               :key #'symbol-name
-                               :test #'string=))))
-       (unless info
-         (error 'bitcoin-error :message (format nil "Currency ~a not supported." currency)))
-       (values
-        (cdr (assoc :|15M| info))
-        (cdr (assoc :last info))
-        (cdr (assoc :buy info))
-        (cdr (assoc :sell info)))))))
-
-(defun bitcoin-exchange (amount from to)
-  (let* ((from-rate (bitcoin-retrieve-currency-info from))
-         (to-rate (bitcoin-retrieve-currency-info to)))
-    (when (or (null from-rate) (null to-rate))
-      (error 'bitcoin-error :message "Currency not supported."))
-    (float (* (/ amount (rationalize from-rate)) (rationalize to-rate)))))
+(defun retrieve-bitstamp-info ()
+  (let* ((info (bitcoin-request "http://bitstamp.net/api/ticker/")))
+    (values
+     (cdr (assoc :last info))
+     (cdr (assoc :high info))
+     (cdr (assoc :low info))
+     (cdr (assoc :vwap info))
+     (cdr (assoc :volume info))
+     (cdr (assoc :bid info))
+     (cdr (assoc :ask info)))))
 
 (defmodule bitcoin bitcoin-module ("btc"))
 
 (defmethod handle-command ((module bitcoin-module)
                            (cmd (eql 'btc))
                            message args)
-  ".btc - list supported currencies
-.btc <currency> - show exchange rates on BTC for currency
-.btc <amount> BTC - convert amount BTC to USD
-.btc <amount> <currency> - convert amount in currency to BTC
-.btc <amount> <from currency> <to currency> - convert currencies via BTC
-"
+  ".btc - show bitcoin trading information"
   (handler-case
-      (cond
-        ((null args)
-         ;; no arguments - list currencies
-         (reply-to message "Available currencies: ~{~a~^ ~}"
-                   (bitcoin-retrieve-currencies)))
-        ((null (cdr args))
-         ;; one argument - get currency info
-         (handler-case
-             (multiple-value-bind (last buy sell avg)
-                 (bitcoin-retrieve-currency-info (first args))
-               (reply-to message "~a: last:~a buy:~a sell:~a avg:~a"
-                         (string-upcase (first args)) last buy sell avg))
-           (bitcoin-error (e)
-             (reply-to message "Error: ~a" (message-of e)))))
-        ((null (cddr args))
-         ;; two arguments - convert $1 BTC to $2 currency, unless $2 is
-         ;; BTC, then convert $1 USD to BTC.
-         (cond
-           ((string-equal (second args) "btc")
-            (reply-to message "~a BTC => ~f USD"
-                      (first args)
-                      (bitcoin-exchange (parse-number (first args)) "BTC" "USD")))
-           (t
-            (reply-to message "~a ~a => ~f BTC"
-                      (first args)
-                      (string-upcase (second args))
-                      (bitcoin-exchange (parse-number (first args))
-                                        (string-upcase (second args))
-                                        "BTC")))))
-        (t
-         ;; three arguments - convert $1 of $2 currency into $3 currency
-         (let ((from-currency (string-upcase (second args)))
-               (to-currency (string-upcase (third args))))
-           (reply-to message "~a ~a => ~f ~a"
-                     (first args)
-                     from-currency
-                     (bitcoin-exchange (parse-number (first args))
-                                       from-currency
-                                       to-currency)
-                     to-currency))))
+      (multiple-value-bind (last buy sell avg vol bid ask)
+          (retrieve-bitstamp-info)
+        (reply-to message "last:~a high:~a low:~a avg:~a volume:~a bid:~a ask:~a"
+                  last buy sell avg vol bid ask))
     (bitcoin-error (e)
-      (reply-to message "Error: ~a" (message-of e)))
-    (simple-type-error (e)
-      (declare (ignore e))
-      ;; triggered by floating point overflow in sbcl
-      (reply-to message "Error: Amount is completely insane"))
-    (parse-error (e)
-      (reply-to message "~a." e))))
+      (reply-to message "Error: ~a" (message-of e)))))
