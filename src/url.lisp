3c2a88ad4979ecd4a0a789f98f137e88a08d740e
Daniel Lowe <dlowe@dlowe.net>
2016-10-07 11:01:48 -0400

    Emit summary when url found in privmsg using url module

--- src/url.lisp
+++ src/url.lisp
@@ -29,6 +29,44 @@
   (write-to-file (data-path "urls.lisp")
                  (urls-of module)))
 
+(defun generic-uri-summary (response headers)
+  (multiple-value-bind (type subtype)
+      (drakma:get-content-type headers)
+    (alexandria:switch ((concatenate 'string type "/" subtype) :test #'string=)
+      ("text/html"
+       (let* ((dom (plump:parse response))
+              (titles (and dom (plump:get-elements-by-tag-name dom "title"))))
+         (when titles
+           (plump:text (plump:first-child (elt titles 0))))))
+      ("text/plain"
+       (string-limit response 100)))))
+
+(defun retrieve-uri-summary (uri)
+  (handler-case
+      (multiple-value-bind (response status headers)
+          (drakma:http-request uri :want-stream t :connection-timeout 2)
+        (when (= status 200)
+          ;; on success read into our buffer
+          (let* ((buf (make-string 8196))
+                 (len (read-sequence buf response)))
+            (close response)
+            (generic-uri-summary (subseq buf 0 len) headers))))
+    (usocket:ns-host-not-found-error ()
+      "ERROR: Host not found")))
+
+(defmethod examine-message ((module url-module)
+                            (message irc:irc-privmsg-message))
+  (ppcre:do-matches-as-strings  (uri-string "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)" (second (arguments message)) nil :sharedp t)
+    (when (< (length uri-string) 2083)
+      ;; save url
+      (pushnew uri-string (urls-of module) :test #'string=)
+      (save-urls module)
+      ;; query url and emit summary
+      (let* ((uri (puri:parse-uri uri-string))
+             (summary (retrieve-uri-summary uri-string)))
+        (when summary
+          (reply-to message "[~a] - ~a" summary (puri:uri-host uri)))))))
+
 (defmethod handle-command ((module url-module)
                            (cmd (eql 'url))
                            message args)
